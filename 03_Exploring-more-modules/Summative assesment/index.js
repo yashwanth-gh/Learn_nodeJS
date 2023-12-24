5c3190a4bee26780d5d0d1110b617386fbe4586e
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-24 22:33:51 +0530

    Day-9 Learnt Event listeners

---
+++ 03_Exploring-more-modules/Summative assesment/index.js
@@ -0,0 +1,54 @@
+import http from "http";
+import fs from "fs";
+import EventEmitter from "events";
+import nodemailer from "nodemailer";
+
+class CustomEvent extends EventEmitter {
+  mailSent(email) {
+    this.emit("mailSent", email);
+  }
+}
+
+const customEvent = new CustomEvent();
+
+const server = http.createServer((req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "codingninjas2k16@gmail.com",
+      pass: "slwvvlczduktvhdj",
+    },
+  });
+
+  if (req.method === "POST") {
+    let data = "";
+
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    req.on("end", () => {
+      const { name, email, message } = JSON.parse(data);
+
+      // TODO: Store user query in "query.txt"
+      // TODO: Use Nodemailer to send confirmation email
+      // TODO: Emit "mailSent" event
+
+      res.end("Query received");
+    });
+  } else {
+    res.end("Welcome to Coding Ninjas!");
+  }
+});
+
+const Solution = () => {
+  customEvent.addListener("mailSent", (email) => {
+    console.log(`custom event "mailSent" emitted`);
+    console.log(
+      `confirming that the email has been sent successfully to ${email}`
+    );
+  });
+};
+
+export default server;
+export { server, CustomEvent, Solution };
